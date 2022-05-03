2d51f7dafba843c76521c3f49e761260b038dd8c
alexbenavente-cloudreach <alex.benavente@cloudreach.com>
2022-05-03 16:34:21 -0400

    merging branches

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 data "aws_ami" "aws_ubuntu" {
     owners = [var.aws_owner_id]
     most_recent = true
@@ -23,6 +24,8 @@ data "aws_subnet" "public"{
 }
 
 =======
+=======
+>>>>>>> 8439d14 (merging branches)
 # DATA SOURCE FOR AMI
 data "aws_ami" "aws_basic_ubuntu" {
   owners      = [var.aws_owner_id]
@@ -54,4 +57,7 @@ data "aws_subnet" "private_a" {
     values = [var.private_subnet_name]
   }
 }
+<<<<<<< HEAD
+>>>>>>> 8439d14 (merging branches)
+=======
 >>>>>>> 8439d14 (merging branches)
